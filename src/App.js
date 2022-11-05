7f2fa768ffcf79e82bda5382f43591744330de63
James Odiwuor <jamespengo95@gmail.com>
2022-11-05 18:22:31 +0300

    route: create the default home route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Home from './Components/Home';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 
 
 function App() {
   return (
     <>
-    <Home/>
+      <Router> 
+        <Routes> 
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </Router>
     </>
   );
 }
