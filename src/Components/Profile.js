18c66d8595a161bdefc6238231db2facb6994c09
James Odiwuor <jamespengo95@gmail.com>
2022-11-05 16:46:30 +0300

    name: correct twitter username

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -8,7 +8,7 @@ function Profile() {
         <div className='profile-container'>
             <div className='profile'> 
             <img id='profile_img' className='profile-pic' src='images/andrew.jpg' alt='Profile'/>
-            <p id='twitter' className='twitter-name'>James Odiwuor</p>
+            <p id='twitter' className='twitter-name'>@jamesodiwuor2</p>
             <p id='slack' className='slack-name'>pengoJames</p>
             </div>
 
